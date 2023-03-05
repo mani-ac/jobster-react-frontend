68edbcb1ce0ea74062105d1c01125589bff0e267
mani <manidharmalingam98@gmail.com>
2023-03-05 18:04:14 +0530

    Initial Commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Offal mumblecore asymmetrical, seitan marfa slow-carb bicycle rights
+            everyday carry la croix kale chips. Helvetica pickled bespoke air
+            plant palo santo. Master cleanse glossier readymade woke actually.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
